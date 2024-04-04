14015f1f3326e4c11a0f2fca3c685320b0d7bf06
Curtis <curtis@poteris.co.uk>
2024-04-04 14:18:04 +0100

    Handle inability to connect to Redis in meetings index

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -4,10 +4,14 @@ class MeetingsController < ApplicationController
   def index
     @meetings = Meeting.includes(:council, :documents, :committee).order(date: :desc).limit(100)
 
-    queue = Sidekiq::Queue.new # Default queue. You can specify a different queue by passing its name as an argument.
-    @council_job_count = queue.select { |job| job.klass == 'ScrapeCouncilWorker' }.count
-    queue = Sidekiq::Queue.new # Default queue. You can specify a different queue by passing its name as an argument.
-    @meeting_job_count = queue.select { |job| job.klass == 'ScrapeMeetingWorker' }.count
+    begin
+      queue = Sidekiq::Queue.new # Default queue. You can specify a different queue by passing its name as an argument.
+      @council_job_count = queue.select { |job| job.klass == 'ScrapeCouncilWorker' }.count
+      @meeting_job_count = queue.select { |job| job.klass == 'ScrapeMeetingWorker' }.count
+    rescue RedisClient::CannotConnectError
+      @council_job_count = 'unknown'
+      @meeting_job_count = 'unknown'
+    end
   end
   
   def show
